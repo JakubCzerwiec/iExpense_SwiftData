695d11e7a841c04f6cd539d8828947b1f0dad6e7
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-11-20 04:08:15 +0100

    Initial Commit

---
+++ iExpense_SwiftData/iExpense_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_SwiftDataApp.swift
+//  iExpense_SwiftData
+//
+//  Created by Jakub Czerwiec  on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
